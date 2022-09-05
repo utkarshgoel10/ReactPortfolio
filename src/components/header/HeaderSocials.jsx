9320a985be6ee40695f69dee9f6f212ea23b7d7b
Utkarsh Goel <66838142+utkarshgoel10@users.noreply.github.com>
2022-09-06 01:00:51 +0530

    Update HeaderSocials.jsx

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {BsLinkedin} from 'react-icons/bs'
 import {FaGithub} from 'react-icons/fa'
 import {BsInstagram} from 'react-icons/bs'
+import {GrYoutube} from 'react-icons/gr'
 import {Link} from 'react-router-dom'
 const HeaderSocials = () => {
   return (
@@ -9,6 +10,7 @@ const HeaderSocials = () => {
         <a href="https://www.linkedin.com/in/goelutkarsh/"><BsLinkedin /></a>
         <a href="https://github.com/utkarshgoel10"><FaGithub /></a>
         <a href="https://www.instagram.com/utkarsh_goel.10/"><BsInstagram /></a>
+        <a href="https://www.youtube.com/channel/UCStJJmtgJnLoTKBRi9cOQSg"><GrYoutube /></a>
     </div>
   )
 }
